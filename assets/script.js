59e427ad81b8d3567663e29702060786c4753d00
Brandon <110788407+bkorbsquare@users.noreply.github.com>
2022-11-11 15:33:33 -0600

    Trying to condense passwordContent section

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,6 @@ var upperCases = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 var passwordContent = [''];
 var passwordContentStart = [''];
-var scopeNumbers = ['8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100','101','102','103','104','105','106','107','108','109','110','111','112','113','114','115','116','117','118','119','120','121','122','123','124','125','126','127','128']
 
 // Write password to the #password input
 function writePassword() {
@@ -22,14 +21,8 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword () {
   var passwordLength = window.prompt ("Please enter the number of characters you want. Passwords must contain more than 7 and less than 129 characters.");
   console.log(passwordLength);
-  if (passwordLength < 8) {
-    window.alert ("Your password must contain more than 7 characters.");
-    return
-  } else if (passwordLength > 128) {
-    window.alert ("Your password must contain less than 129 characters.");
-    return
-  } else if (passwordLength != scopeNumbers) {
-    window.alert ("You must select a numeric value greater than 7 and less than 129.");
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    window.alert ("Your password must contain a numeric value more than 7 and less than 129.");
     return
   } else {
     window.alert ("Your password will be " + passwordLength + " characters long.");
@@ -69,6 +62,22 @@ function generatePassword () {
     window.alert ("Your password will not contain special characters.");
   }
 
+  if (hasNumbers === true) {
+    passwordContent.concat (numbers)
+  }
+
+  if (hasLowerCases === true) {
+    passwordContent.concat (lowerCases)
+  }
+
+  if (hasUpperCases === true) {
+    passwordContent.concat (upperCases)
+  }
+
+  if (hasSpecials === true) {
+    passwordContent.concat (specials)
+  }
+/*
   if (hasNumbers === true && hasLowerCases === true && hasUpperCases === true && hasSpecials === true) {
     var passwordContent = passwordContentStart.concat (numbers, lowerCases, upperCases, specials)
   }
@@ -128,6 +137,7 @@ function generatePassword () {
   if (hasNumbers === true && hasLowerCases === false && hasUpperCases === true && hasSpecials === true) {
     var passwordContent = passwordContentStart.concat (numbers, upperCases, specials)
   }
+  */
 
   if (!numbers && !lowerCases && !upperCases && !specials) {
     window.alert ("You must select at least one variable.")
